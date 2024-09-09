b9855972ceb6bd7c7c7fbd66fb993c57b4fb40fd
Andra <andarbuz@gmail.com>
2024-09-09 18:07:12 +0300

    created conference resolvers

---
+++ src/features/conference/query.resolvers.js
@@ -0,0 +1,42 @@
+const { prisma } = require('../../prisma')
+const conferenceQueryResolvers = {
+  Query: {
+    conferenceList: (_parent, { filters }, _ctx, _info) => {
+      let localWhere = undefined
+      if (filters) localWhere = {}
+      if (filters?.startDate) localWhere.startDate = { gte: filters.startDate }
+      if (filters?.endDate) localWhere.endDate = { lte: filters.endDate }
+      return prisma().conference.findMany({ where: localWhere })
+    }
+  },
+  Conference: {
+    type: ({ conferenceTypeId }) => {
+      return prisma().dictionaryConferenceType.findUnique({ where: { id: conferenceTypeId } })
+    },
+    category: ({ categoryId }) => {
+      return prisma().dictionaryCategory.findUnique({ where: { id: categoryId } })
+    },
+    location: ({ locationId }) => {
+      return prisma().location.findUnique({ where: { id: locationId } })
+    },
+    status: async ({ id }, { userEmail }) => {
+      const result = await prisma().conferenceXAttendee.findUnique({
+        where: { conferenceId: id, attendeeEmail: userEmail },
+        include: { dictionaryStatus: true }
+      })
+      return result?.dictionaryStatus
+    }
+  },
+  Location: {
+    city: ({ cityId }) => {
+      return prisma().dictionaryCity.findUnique({ where: { id: cityId } })
+    },
+    county: ({ countyId }) => {
+      return prisma().dictionaryCounty.findUnique({ where: { id: countyId } })
+    },
+    country: ({ countryId }) => {
+      return prisma().dictionaryCountry.findUnique({ where: { id: countryId } })
+    }
+  }
+}
+module.exports = conferenceQueryResolvers
